00702909d30bf10851761903d65af1a1ff1de565
Richard <richytong@gmail.com>
2019-01-29 12:27:45 -0800

    feat: search

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # wweb
-Simple, isomorphic interface around common Web APIs. Perfect for intuitively interacting with your browser. Tired of using `window`? Don't know/care if you're in SERVER or BROWSER? This may interest you.
+Simple, isomorphic interface around common Web APIs, perfect for intuitively interacting with your browser. Built for modern browsers. Tired of using `window`? Don't know/care if you're in SERVER or BROWSER? This may interest you.
 
 # Installation
 ```bash
@@ -24,37 +24,54 @@ app.get('/*', (req, res) => {
 /* anywhere.jsx? */
 const wweb = require('wweb')
 
-// redirect to myawesomewebsite.com
 wweb.redirect('https://myawesomewebsite.com')
+// redirects to myawesomewebsite.com, same as window.location = '...'
 
-// gets the current page from query params ?my_query_param=something
-const myQueryParam = wweb.search.get('my_query_param')
+console.log(window.location.search)
+// => '?foo=bar'
+
+const myQueryParam = wweb.search.get('foo')
+// => 'bar'
 
 // gets entire search query as an object
 const search = wweb.search.getAll()
+// => { foo: 'bar' }
 
-// removes my_query_param from the query params
-wweb.search.remove('my_query_param')
+wweb.search.remove('foo')
+console.log(window.location.search)
+// => ''
+
+wweb.search.set('foo', 'bar')
+wweb.search.set('baz', 'qux')
+console.log(window.location.search)
+// => '?foo=bar&baz=qux'
 
 // clears entire search query
-wweb.search.removeAll()
 wweb.search.clear()
+console.log(window.location.search)
+// => ''
 
-// gets my_cookie from the document cookies
-const myCookie = wweb.cookies.get('my_cookie')
+console.log(window.document.cookie)
+// => 'foo=bar; baz=qux'
 
-// gets all cookies from document
+const myCookie = wweb.cookies.get('foo')
+// => 'bar'
+
 const cookies = wweb.cookies.getAll()
+// => { foo: 'bar', baz: 'qux' }
 
-// removes my_cookie from the document cookies
-wweb.cookies.remove('my_cookie')
+wweb.cookies.remove('foo')
+console.log(window.document.cookie)
+// => 'baz=qux'
 
-// clears all cookies from document
-wweb.cookies.removeAll()
+wweb.cookies.set('foo', 'bar')
+console.log(window.document.cookie)
+// => 'foo=bar; baz=qux'
+
 wweb.cookies.clear()
+console.log(window.document.cookie)
+// => ''
 ```
 
-# License/Author
+# License
 <b>wweb</b> © [richytong](https://github.com/richytong)
-
-Created and maintained by richytong.
--- src/index.js
+++ src/index.js
@@ -1,4 +1,14 @@
+const queryString = require('query-string')
+
 let SERVER_SEARCH = {}
+
+const cleanHash = (hash) => {
+  const dirtyHashPos = hash.indexOf('?')
+  if (dirtyHashPos > -1) return hash.slice(0, dirtyHashPos)
+  return hash
+}
+
+const generateRelativeUrl = (pathname, qs, hash) => `${pathname}?${qs}${cleanHash(hash)}`
 
 module.exports = {
   init: (config) => {
@@ -9,30 +19,64 @@ module.exports = {
   },
   redirect: (url) => {
     if (typeof window !== 'object') return
-    window.location = url
+    window.location.replace(url)
   },
   search: {
     get: (name) => {
       if (!name) return null
       if (typeof window !== 'object') return SERVER_SEARCH[name] || null
       if (!window.location.search) return null
-      const eqpairs = decodeURIComponent(window.location.search).slice(1).split('&')
-      for (let i = 0; i < eqpairs.length; i++) {
-        const [sname, sval] = eqpairs[i].split('=')
-        if (sname === name) return sval
-      }
-      return null
+      const searchObj = queryString.parse(window.location.search)
+      return searchObj[name] || null
     },
     getAll: () => {
       if (typeof window !== 'object') return SERVER_SEARCH
       if (!window.location.search) return {}
-      const searchObj = {}
-      const eqpairs = decodeURIComponent(window.location.search).slice(1).split('&')
-      for (let i = 0; i < eqpairs.length; i++) {
-        const [sname, sval] = eqpairs[i].split('=')
-        searchObj[sname] = sval
+      return queryString.parse(window.location.search)
+    },
+    set: (name, value) => {
+      if (!name || !value) return
+      if (typeof window !== 'object') {
+        SERVER_SEARCH[name] = value
+        return
       }
-      return searchObj
+      const searchObj = queryString.parse(window.location.search)
+      searchObj[name] = value
+      const relUrl = generateRelativeUrl(
+        window.location.pathname,
+        queryString.stringify(searchObj),
+        window.location.hash,
+      )
+      window.history.pushState({}, '', relUrl)
+    },
+    remove: (name) => {
+      if (!name) return
+      if (typeof window !== 'object') {
+        delete SERVER_SEARCH[name]
+        return
+      }
+      if (!window.location.search) return
+      const searchObj = queryString.parse(window.location.search)
+      delete searchObj[name]
+      const relUrl = generateRelativeUrl(
+        window.location.pathname,
+        queryString.stringify(searchObj),
+        window.location.hash,
+      )
+      window.history.pushState({}, '', relUrl)
+    },
+    clear: () => {
+      if (typeof window !== 'object') {
+        SERVER_SEARCH = {}
+        return
+      }
+      if (!window.location.search) return
+      const relUrl = generateRelativeUrl(
+        window.location.pathname,
+        '',
+        window.location.hash,
+      )
+      window.history.pushState({}, '', relUrl)
     },
   },
 }
